f04562a1cda3e458c4660ec6d28e9d9cf0a45d0c
AlejandroCis12 <alejandro240915@gmail.com>
2025-06-25 21:43:09 -0600

    codigo de takagi chido

--- funcion.py
+++ funcion.py
@@ -5,11 +5,11 @@ def T(x, N=500):
     suma = 0
     for n in range(N):
         tx = 2**n * x
-        diente = 2 * min(tx/2 - np.floor(tx/2), np.ceil(tx/2) - tx/2)
+        diente = 2 * min(tx - np.floor(tx), np.ceil(tx) - tx)
         suma += (1/2)**n * diente
-    return 0.75 - suma
+    return suma
 
-def j_newton(fun, q, x0, tol, nmax):
+def j_newton(fun, a, x0, tol, nmax):
     """
     Newton-Jackson method to find roots of a function.
 
@@ -28,23 +28,20 @@ def j_newton(fun, q, x0, tol, nmax):
     """
     curve = []
     x = x0
-    fx = fun(x)
-    fqx = fun(q * x)
+    fx = fun(x)-a
     curve.append((x, fx))
     niter = 0
     diff = tol + 1
 
     while diff >= tol and niter <= nmax:
         niter += 1
-        dfqx = fx / fqx if fqx != 0 else float('inf')  # Avoid division by zero
-        denominator = 1 - dfqx
+        denominator = fun(x)-x
         if denominator == 0:
             print("Denominator zero. Method stopped to avoid division by zero.")
             break
-        x = x * (1 - q * dfqx) / denominator
-        fx = fun(x)
-        fqx = fun(q * x)
-        diff = abs(fx)
+        x = x * (1-((fun(x)-a)/denominator))
+        fx = fun(x)-a
+        diff = abs(fun(x))
         curve.append((x, fx))
 
     if niter > nmax:
@@ -54,6 +51,17 @@ def j_newton(fun, q, x0, tol, nmax):
     res = fx
     return zero, res, niter, curve
 
+a=0.34
 
-
-
+Jz, Jr, Jn, Jc = j_newton(fun=T, a = 0.34, x0=.2, tol=1e-5, nmax=500)
+Lx = np.linspace(0, 1, 1000)
+Ly = [T(x)-a for x in Lx]
+plt.figure(figsize=(8, 6))
+plt.plot(Lx, Ly, linewidth=1.2, label='f(x)')
+Jc = np.array(Jc)
+plt.plot(Jc[:, 0], Jc[:, 1], '.-', linewidth=1.2, markersize=8, label='Iteraciones')
+plt.grid(True)
+plt.title("Convergence of Newton method with Jackson derivative")
+plt.legend()
+print(f"\nCon la derivada de jackson se encontró la raíz {Jz:.6f} en {Jn} iteraciones f({Jz:.6f}) = {Jr:.6e}")
+plt.show()
